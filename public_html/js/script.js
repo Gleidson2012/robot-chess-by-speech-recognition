0e5a7ff2e782ba15714a44a770b5eceef8b55d05
Gleidson Brito Santana <gleidsonbrito.ads@gmail.com>
2015-11-20 03:57:52 -0200

    Integração com Arduino via Websocket.

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -19,8 +19,9 @@ var makeRandomMove = function () {
             return;
 
         var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-        game.move(possibleMoves[randomIndex]);
+        move = game.move(possibleMoves[randomIndex]);
         board.position(game.fen());
+        websocket.send("send /dev/ttyACM0 " + JSON.stringify(move) + '\n');
     }
 };
 
@@ -52,7 +53,7 @@ var cfg = {
     onDragStart: onDragStart,
     onDrop: onDrop,
     onSnapEnd: onSnapEnd,
-    pieceTheme: 'chessboardjs/www/img/chesspieces/alpha/{piece}.png',
+    pieceTheme: baseUrl + '/js/chessboardjs/www/img/chesspieces/alpha/{piece}.png',
 };
 board = ChessBoard('board', cfg);
 
@@ -74,6 +75,7 @@ var movePiece = function (from, to) {
         return 'snapback';
 
     board.move(algebraicNotationCoordinateFrom.concat('-').concat(algebraicNotationCoordinateTo));
+    websocket.send("send /dev/ttyACM0 " + JSON.stringify(move));
     // make random legal move for black
     window.setTimeout(makeRandomMove, 250);
 };
@@ -96,7 +98,14 @@ var undo = function (numberOfMoviments) {
             break;
     }
     makeRandomMove();
-}
+};
+
+var disconnectArduino = function(){
+    websocket.send('close /dev/ttyACM0');
+};
+var connectArduino = function(){
+    websocket.send('open /dev/ttyACM0 9600');
+};
 
 var convertNumberInWordsToNumeral = function (number) {
     switch (number) {
@@ -143,11 +152,24 @@ if (annyang) {
         '(mover de) :coordenada1 para :coordenada2': movePiece,
         'novo jogo': newGame,
         'desfazer': undo,
-        'desfazer :n movimentos': undo
+        'desfazer :n movimentos': undo,
+        'fechar (a) porta': disconnectArduino,
+        'abrir (a) porta': connectArduino
     };
 
     annyang.addCommands(commands);
     annyang.setLanguage('pt-BR');
     annyang.debug();
     annyang.start();
+}
+
+var websocket = new WebSocket('ws://192.168.1.34:8989/ws');
+websocket.onopen = function (event) {
+    websocket.send("open /dev/ttyACM0 9600");
+};
+websocket.onerror = function (e) {
+    console.log('Error with WebSocket uid: ' + e.target.uid);
+};
+websocket.onmessage = function (e) {
+    console.log(e.data);
 }
